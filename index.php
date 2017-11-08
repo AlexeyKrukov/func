40064b3b140f7641a16a420d81884a94b970a290
AlexeyKrukov <krukovworkmail@gmail.com>
2017-11-09 01:53:04 +0300

    second commit: PDO added

--- index.php
+++ index.php
@@ -14,20 +14,55 @@
         echo $result;
     }
     function get_news($link, $id){
-        $query = "SELECT * FROM news WHERE id = {$id}";
-        $result = mysqli_query($link, $query);
-        if(!$result)
-            die(mysqli_error($link));
-        $n = mysqli_num_rows($result);
-        $arr = array();
-        for($i = 0; $i < $n; $i++){
-            $row = mysqli_fetch_assoc($result);
-            $arr[] = $row;
+        $query = htmlentities("SELECT * FROM news WHERE id = ?");
+        $pdo = new PDO('mysql:host=localhost;dbname=informations','root','');
+        $stmt = $pdo->prepare($query);
+        $stmt->execute(array($id));
+        $arr = $stmt->fetchAll();
+        $new = array();
+        $n = 0;
+        for($i = 0; $i < count($arr[0]) - 5; $i++){
+            $key = array_search($arr[0][$i], $arr[0]);
+            if(is_numeric($key));
+            else {
+                $new[$key] = $arr[0][$i];
+                $n++;
+            }
         }
-        return json_encode($arr[0]);
+        //print_r($new);
+        return json_encode($new);
+        //print_r($arr[0][1]);
+            /*$result = mysqli_query($link, $query);
+            if(!$result)
+                die(mysqli_error($link));
+            $n = mysqli_num_rows($result);
+            $arr = array();
+            for($i = 0; $i < $n; $i++){
+                $row = mysqli_fetch_assoc($result);
+                $arr[] = $row;
+            }
+        print_r($arr[0]);
+            return json_encode($arr[0]);*/
     }
     function get_publications($link, $id){
-        $query = "SELECT * FROM publications WHERE id = {$id}";
+        $query = htmlentities("SELECT * FROM publications WHERE id = ?");
+        $pdo = new PDO('mysql:host=localhost;dbname=informations','root','');
+        $stmt = $pdo->prepare($query);
+        $stmt->execute(array($id));
+        $arr = $stmt->fetchAll();
+        $new = array();
+        $n = 0;
+        for($i = 0; $i < count($arr[0]) - 6; $i++){
+            $key = array_search($arr[0][$i], $arr[0]);
+            if(is_numeric($key));
+            else {
+                $new[$key] = $arr[0][$i];
+                $n++;
+            }
+        }
+        //print_r($new);
+        return json_encode($new);
+        /*$query = "SELECT * FROM publications WHERE id = {$id}";
         $result = mysqli_query($link, $query);
         if(!$result)
             die(mysqli_error($link));
@@ -37,6 +72,6 @@
             $row = mysqli_fetch_assoc($result);
             $arr[] = $row;
         }
-        return json_encode($arr[0]);
+        return json_encode($arr[0]);*/
     }
 ?>
